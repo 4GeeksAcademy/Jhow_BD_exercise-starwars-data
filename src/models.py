3f20472b1ef3336de5d7a45df239e30fbf4ab9fd
Jhowmsm <jhownsman@gmail.com>
2024-10-21 21:28:43 +0000

    relaciones finalizadas

--- src/models.py
+++ src/models.py
@@ -1,29 +1,67 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(250), unique=True, nullable=False)
+    firstname = Column(String(70) )
+    lastname = Column(String(60) )
+    address = Column(String(80) )
+    phone = Column(String(80) )
+    password = Column(String(60) )
+    email = Column(String(250), unique=True,  nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+    user = relationship('Likes', back_populates='user')
+
+#ok
+class Planetas(Base):
+    __tablename__ = 'planetas'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(250), unique=True, nullable=False)
+    gravedad = Column(String(70))
+    diametro = Column(String(60))
+    poblacion = Column(String(80))
+    descripcion = Column(String(220))
+#
+    planetas = relationship('Likes', back_populates='planetas')
+    
+#ok
+class Personajes(Base):
+    __tablename__ = 'personajes'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(250), unique=True, nullable=False)
+    peso = Column(String(70) )
+    color_ojos = Column(String(60) )
+    sexo = Column(Enum('Hombre','Mujer','No definido'), nullable=False)
+    color_pelo = Column(String(50) )
+
+    personajes = relationship('Likes', back_populates='personajes')
+
+class Likes(Base):
+    __tablename__ = 'likes'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id_planetas = Column(Integer, ForeignKey('planetas.id'), primary_key=True)
+    id_personajes = Column(Integer, ForeignKey('personajes.id'), primary_key=True)
+    id_user = Column(Integer, ForeignKey('user.id'), primary_key=True)
+
+    likes = relationship('Planetas', back_populates='likes')
+    likes = relationship('Personajes', back_populates='likes')
+    likes = relationship('User', back_populates='likes')
+    
 
     def to_dict(self):
         return {}
